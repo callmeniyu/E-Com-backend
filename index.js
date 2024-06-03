f7844c32c37eb261259c365406cac6cceddf35ce
callmeniyu <getniyashere@gmail.com>
2024-06-03 13:51:43 +0530

    test changes made to multer url for deployment

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ app.use("/images", express.static("upload/images"))
 app.post("/upload", upload.single("product"), (req, res) => {
     res.json({
         success: true,
-        image_url: `${process.env.VITE_SERVER_URL}images/${req.file.filename}`, 
+        image_url: `https://e-com-admin-hoxc.onrender.com/images/${req.file.filename}`, 
     })
 })
 
